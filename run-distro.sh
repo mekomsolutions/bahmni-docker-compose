0ae12bc0b25e35b543591615d233f9b29e8d2ee2
Zoheir CHINE <zoheir@mekomsolutions.com>
2023-05-25 08:49:37 +0000

    Run distro from target foloder

--- run-distro.sh
+++ run-distro.sh
@@ -3,10 +3,29 @@ set -e
 current_dir=`pwd`
 DISTRO_NAME=$1
 DISTRO_VERSION=$2
-distro_path=`mktemp -d -t $DISTRO_NAME-XXXXX`
+distro_path=$current_dir/target/$DISTRO_NAME
+profile="$1-$2"
+profile=`echo $profile | sed -r 's/[/.]+/_/g'`
+docker_compose=""
+
+if command -v docker-compose &> /dev/null; then
+    docker_compose="docker-compose"
+elif docker compose ps; then
+    docker_compose="docker compose"
+else
+    echo "Docker Compose not found, aborting."
+    exit
+fi
+
+echo "⚙️  Cleanup and prepare for distro run"
+rm -rf $distro_path
+$docker_compose -p $profile down -v
+
+echo "⚙️  Fetching distro..."
 ./mvnw org.apache.maven.plugins:maven-dependency-plugin:3.2.0:get -DremoteRepositories=https://nexus.mekomsolutions.net/repository/maven-public -Dartifact=net.mekomsolutions:bahmni-distro-$DISTRO_NAME:$DISTRO_VERSION:zip -Dtransitive=false
 ./mvnw org.apache.maven.plugins:maven-dependency-plugin:3.2.0:unpack -Dmdep.overWriteReleases=true -Dmdep.overWriteSnapshots=true -Dartifact=net.mekomsolutions:bahmni-distro-$DISTRO_NAME:$DISTRO_VERSION:zip -DoutputDirectory=$distro_path
 
+echo "⚙️ Starting distro..."
 export DISTRO_PATH=$distro_path && \
 export OPENMRS_CONFIG_PATH=$DISTRO_PATH/openmrs_config && \
 export BAHMNI_CONFIG_PATH=$DISTRO_PATH/bahmni_config && \
@@ -16,5 +35,4 @@ export ODOO_CONFIG_PATH=$DISTRO_PATH/odoo_config && \
 export ODOO_EXTRA_ADDONS=$DISTRO_PATH/odoo_addons && \
 export EIP_CONFIG_PATH=$DISTRO_PATH/eip_config
 
-profile=`echo $RANDOM | md5sum | head -c 5; echo;`
-docker-compose -p $profile up
+$docker_compose -p $profile up
