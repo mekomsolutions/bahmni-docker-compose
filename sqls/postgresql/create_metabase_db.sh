96238e711b635f9468f7b82066e12f3ad5001fdd
Emmanuel Nyachoke <achachiez@gmail.com>
2024-04-15 17:48:50 +0300

    HSC-250: Upgrade PostgreSQL image to 13 to support Ozone Analytics

--- sqls/postgresql/create_metabase_db.sh
+++ sqls/postgresql/create_metabase_db.sh
@@ -8,7 +8,7 @@ function create_user_and_database() {
 	local password=$3
 	echo "  Creating '$user' user and '$database' database..."
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" $POSTGRES_DB <<-EOSQL
-	    CREATE USER $user WITH UNENCRYPTED PASSWORD '$password';
+	    CREATE USER $user WITH PASSWORD '$password';
 	    CREATE DATABASE $database;
 	    GRANT ALL PRIVILEGES ON DATABASE $database TO $user;
 EOSQL
--- sqls/postgresql/create_odoo_db.sh
+++ sqls/postgresql/create_odoo_db.sh
@@ -7,7 +7,7 @@ function create_user() {
 	local password=$2
 	echo "  Creating '$user' user..."
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" $POSTGRES_DB <<-EOSQL
-	    CREATE USER $user WITH UNENCRYPTED PASSWORD '$password';
+	    CREATE USER $user WITH PASSWORD '$password';
 	    ALTER USER $user CREATEDB;
 EOSQL
 }
